fd15129529e032bf74b3a52a9380a1d468c4279c
cnMac <cool_navy@qq.com>
2018-05-18 21:02:20 +0800

    code formate

--- Assets/BundleRes/ui/login/login.lua
+++ Assets/BundleRes/ui/login/login.lua
@@ -1,5 +1,3 @@
-
-
 local CS = CS
 local UnityEngine = CS.UnityEngine
 local GameObject = UnityEngine.GameObject
@@ -11,72 +9,84 @@ local util = require "lua.utility.xlua.util"
 local login = {}
 local self = login
 
-local yield_return = util.async_to_sync(function (to_yield, callback)
-    mono:YieldAndCallback(to_yield, callback)
-end)
+local yield_return =
+	util.async_to_sync(
+	function(to_yield, callback)
+		mono:YieldAndCallback(to_yield, callback)
+	end
+)
 
-function login.OpenTest( ... )
-	return coroutine.create(function()
-	    local obj = nil
-		yield_return(CS.AssetSys.Instance:GetAsset("ui/test/test.prefab", function(asset)
-	        obj = asset;
-	    end))
-	    local test = GameObject.Instantiate(obj)
-	    test.name = "test"
-	    local oldLoading = GameObject.Find("loading")
-	    GameObject.DestroyImmediate(oldLoading)
+function login.OpenTest(...)
+	return coroutine.create(
+		function()
+			local obj = nil
+			yield_return(
+				CS.AssetSys.Instance:GetAsset(
+					"ui/test/test.prefab",
+					function(asset)
+						obj = asset
+					end
+				)
+			)
+			local test = GameObject.Instantiate(obj)
+			test.name = "test"
+			local oldLoading = GameObject.Find("loading")
+			GameObject.DestroyImmediate(oldLoading)
 
-	    GameObject.DestroyImmediate(mono.gameObject)
-	end)
+			GameObject.DestroyImmediate(mono.gameObject)
+		end
+	)
 end
 
 --AutoGenInit Begin
 function login.AutoGenInit()
-    login.Login_Button = Login:GetComponent("UnityEngine.UI.Button")
-    login.id_InputField = id:GetComponent("UnityEngine.UI.InputField")
-    login.pwd_InputField = pwd:GetComponent("UnityEngine.UI.InputField")
+	login.Login_Button = Login:GetComponent("UnityEngine.UI.Button")
+	login.id_InputField = id:GetComponent("UnityEngine.UI.InputField")
+	login.pwd_InputField = pwd:GetComponent("UnityEngine.UI.InputField")
 end
 --AutoGenInit End
 
 function login.Awake()
 	self.AutoGenInit()
 
-	self.id_InputField.onEndEdit:AddListener(function ( text )
-		self.pwd_InputField:Select()
-		print("x_InputField.onEndEdit:"..text)
-	end)
-	self.pwd_InputField.onEndEdit:AddListener(function ( text )
-		print("y_InputField.onEndEdit:"..text)
-	end)
+	self.id_InputField.onEndEdit:AddListener(
+		function(text)
+			self.pwd_InputField:Select()
+			print("x_InputField.onEndEdit:" .. text)
+		end
+	)
+	self.pwd_InputField.onEndEdit:AddListener(
+		function(text)
+			print("y_InputField.onEndEdit:" .. text)
+		end
+	)
 
-	self.Login_Button.onClick:AddListener(function ( ... )
-		assert(coroutine.resume(self.OpenTest()))
-	end)
+	self.Login_Button.onClick:AddListener(
+		function(...)
+			assert(coroutine.resume(self.OpenTest()))
+		end
+	)
 end
 
 function login.OnEnable()
-    print("login.OnEnable")
-
+	print("login.OnEnable")
 end
 
 function login.Start()
-    print("login.Start")
+	print("login.Start")
 end
 
 function login.FixedUpdate()
-
 end
 
 function login.Update()
-
 end
 
 function login.LateUpdate()
-
 end
 
 function login.OnDestroy()
-    print("login.OnDestroy")
+	print("login.OnDestroy")
 end
-    
+
 return login
