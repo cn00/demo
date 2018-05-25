5cdb4968bb59cff9bd78aa8a560b7f5b9807c3a8
cn <cool_navy@qq.com>
2018-05-25 19:57:56 +0800

    add JavaUtil

---
+++ Assets/Plugins/Android/JavaUtil.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JavaUtil
+{
+    public static void CallJavaApi(string className, string apiName, params object[] args)
+    {
+        AppLog.d("sdk call: " + className + ":" + apiName);
+        using (AndroidJavaClass cls = new AndroidJavaClass(className))
+        {
+            cls.CallStatic(apiName, args);
+        }
+    }
+}
