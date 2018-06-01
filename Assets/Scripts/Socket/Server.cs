9990f571d0bd602a39fc696f900ebecf0d52fa90
cn <cool_navy@qq.com>
2018-06-01 21:13:01 +0800

    add NetSys & Socket

---
+++ Assets/Scripts/Socket/Server.cs
@@ -0,0 +1,390 @@
+﻿// Copyright © 2017 - MazyModz. Created by Dennis Andersson. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Net
+{
+    /// <summary>
+    /// Handler for when a message was received
+    /// </summary>
+    public class OnMessageReceivedHandler : EventArgs
+    {
+        /// <summary>The client that send the message</summary>
+        private Client _client;
+
+        /// <summary>The message the client sent</summary>
+        private string _message;
+
+        /// <summary>Create a new message received event handler</summary>
+        /// <param name="Client">The client that sent the message</param>
+        /// <param name="Message">The message the client sent</param>
+        public OnMessageReceivedHandler(Client Client, string Message)
+        {
+            this._client = Client;
+            this._message = Message;
+        }
+
+        /// <summary>Get the client that sent the received message</summary>
+        /// <returns>The client that sent the message</returns>
+        public Client GetClient()
+        {
+            return _client;
+        }
+
+        /// <summary>The message that was received from the client</summary>
+        /// <returns>The received message</returns>
+        public string GetMessage()
+        {
+            return _message;
+        }
+
+    }
+
+    /// <summary>
+    /// Handler for when a message was send to a client
+    /// </summary>
+    public class OnSendMessageHandler : EventArgs
+    {
+        /// <summary>The client the message was sent to</summary>
+        private Client _client;
+
+        /// <summary>The message that was sent to the client</summary>
+        private string _message;
+
+        /// <summary>Create a new handler for when a message was sent</summary>
+        /// <param name="Client">The client the message was sent to</param>
+        /// <param name="Message">The message that was sent to the client</param>
+        public OnSendMessageHandler(Client Client, string Message)
+        {
+            this._client = Client;
+            this._message = Message;
+        }
+
+        /// <summary>The client the message was sent to</summary>
+        /// <returns>The client receiver</returns>
+        public Client GetClient()
+        {
+            return _client;
+        }
+
+        /// <summary>The message that was send to the client</summary>
+        /// <returns>The sent message</returns>
+        public string GetMessage()
+        {
+            return _message;
+        }
+    }
+
+    /// <summary>
+    /// Handler for when a client connected
+    /// </summary>
+    public class OnClientConnectedHandler : EventArgs
+    {
+        /// <summary>The client that connected to the server</summary>
+        private Client _client;
+
+        /// <summary>Create a new event handler for when a client connected</summary>
+        /// <param name="Client">The client that connected</param>
+        public OnClientConnectedHandler(Client Client)
+        {
+            this._client = Client;
+        }
+
+        /// <summary>Get the client that was connected</summary>
+        /// <returns>The client that connected </returns>
+        public Client GetClient()
+        {
+            return _client;
+        }
+    }
+
+    /// <summary>
+    /// Handler for when a client disconnects
+    /// </summary>
+    public class OnClientDisconnectedHandler : EventArgs
+    {
+        /// <summary>The client that diconnected</summary>
+        private Client _client;
+
+        /// <summary>Create a new handler for when a client disconnects</summary>
+        /// <param name="Client">The disconnected client</param>
+        public OnClientDisconnectedHandler(Client Client)
+        {
+            this._client = Client;
+        }
+
+        /// <summary>Gets the client that disconnected</summary>
+        /// <returns>The disconnected client</returns>
+        public Client GetClient()
+        {
+            return _client;
+        }
+    }
+
+    ///<summary>
+    /// Object for all listen servers
+    ///</summary>
+    public partial class Server
+    {
+        #region Fields
+
+        /// <summary>The listen socket (server socket)</summary>
+        private Socket _socket;
+
+        /// <summary>The listen ip end point of the server</summary>
+        private IPEndPoint _endPoint;
+
+        /// <summary>The connected clients to the server </summary>
+        private List<Client> _clients = new List<Client>();
+
+        #endregion
+
+        #region Class Events
+
+        /// <summary>Create and start a new listen socket server</summary>
+        /// <param name="EndPoint">The listen endpoint of the server</param>
+        public Server(IPEndPoint EndPoint
+        , SocketType SocketType = SocketType.Stream
+        , ProtocolType ProtocolType = ProtocolType.Tcp)
+        {
+            // Set the endpoint if the input is valid
+            if (EndPoint == null) return;
+            this._endPoint = EndPoint;
+
+            // Create a new listen socket
+            this._socket = new Socket(AddressFamily.InterNetwork, SocketType, ProtocolType);
+
+            Console.WriteLine("Copyright © 2017 - MazyModz. Created by Dennis Andersson. All rights reserved.\n\n");
+            Console.WriteLine("WebSocket Server Started\nListening on {0}:{1}\n", GetEndPoint().Address.ToString(), GetEndPoint().Port);
+
+            // Start the server
+            start();
+        }
+
+        #endregion
+
+        #region Field Getters
+
+        /// <summary>Gets the listen socket</summary>
+        /// <returns>The listen socket</returns>
+        public Socket GetSocket()
+        {
+            return _socket;
+        }
+
+        /// <summary>Get the listen socket endpoint</summary>
+        /// <returns>The listen socket endpoint</returns>
+        public IPEndPoint GetEndPoint()
+        {
+            return _endPoint;
+        }
+
+        /// <summary>Gets a connected client at the given index</summary>
+        /// <param name="Index">The connected client array index</param>
+        /// <returns>The connected client at the index, returns null if the index is out of bounds</returns>
+        public Client GetConnectedClient(int Index)
+        {
+            if (Index < 0 || Index >= _clients.Count) return null;
+            return _clients[Index];
+        }
+
+        /// <summary>Gets a connected client with the given guid</summary>
+        /// <param name="Guid">The Guid of the client to get</param>
+        /// <returns>The client with the given id, return null if no client with the guid could be found</returns>
+        public Client GetConnectedClient(string Guid)
+        {
+            return _clients.Find(i => i.GetGuid() == Guid);
+        }
+
+        /// <summary>Gets a connected client with the given socket</summary>
+        /// <param name="Socket">The socket of the client </param>
+        /// <returns>The connected client with the given socket, returns null if no client with the socket was found</returns>
+        public Client GetConnectedClient(Socket Socket)
+        {
+            return _clients.Find(i => i.GetSocket() == Socket);
+        }
+
+        /// <summary>Get the number of clients that are connected to the server</summary>
+        /// <returns>The number of connected clients</returns>
+        public int GetConnectedClientCount()
+        {
+            return _clients.Count;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Starts the listen server when a server object is created
+        /// </summary>
+        private void start()
+        {
+            GetSocket().Blocking = true;//false;//
+
+            // Bind the socket and start listending
+            GetSocket().Bind(GetEndPoint());
+
+            if(GetSocket().ProtocolType == ProtocolType.Tcp)
+            {
+                GetSocket().Listen(64);
+
+                // Start to accept clients and accept incomming connections 
+                GetSocket().BeginAccept(connectionCallback, null);
+            }
+        }
+
+        /// <summary>
+        /// Stops the listen server 
+        /// </summary>
+        public void Stop()
+        {
+            GetSocket().Close();
+            GetSocket().Dispose();
+        }
+
+        /// <summary>Called when the socket is trying to accept an incomming connection</summary>
+        /// <param name="AsyncResult">The async operation state</param>
+        private void connectionCallback(IAsyncResult AsyncResult)
+        {
+            try
+            {
+                // Gets the client thats trying to connect to the server
+                Socket clientSocket = GetSocket().EndAccept(AsyncResult);
+                clientSocket.Blocking = true;
+
+                // again:
+                // clientSocket.Send(Encoding.Default.GetBytes("password: "));
+                // // Read the handshake updgrade request
+                // byte[] handshakeBuffer = new byte[32];
+                // int handshakeReceived = clientSocket.Receive(handshakeBuffer);
+                // if(handshakeReceived < handshakeBuffer.Length)
+                //     Array.Resize<byte>(ref handshakeBuffer, handshakeReceived);
+                // var pasword = Encoding.Default.GetString(handshakeBuffer).TrimEnd(new []{'\n', '\r', ' ', '\0'});
+                // if(pasword != "654123")
+                // {
+                //    goto again;
+                // }
+
+                //// Get the hanshake request key and get the hanshake response
+                //string requestKey = Helpers.GetHandshakeRequestKey(Encoding.Default.GetString(handshakeBuffer));
+                //string hanshakeResponse = Helpers.GetHandshakeResponse(Helpers.HashKey(requestKey));
+
+                // Create a new client object and add 
+                // it to the list of connected clients
+                Client client = new Client(this, clientSocket);
+                _clients.Add(client);
+
+                // Send the handshake updgrade response to the connecting client 
+                //clientSocket.Send(Encoding.Default.GetBytes(hanshakeResponse));
+                clientSocket.Send(Encoding.Default.GetBytes("Welcom!!! " + client.GetGuid() + "\n"));
+
+                // Call the event when a client has connected to the listen server 
+                if (OnClientConnected == null) 
+                    throw new Exception("Server error: event OnClientConnected is not bound!");
+                OnClientConnected(this, new OnClientConnectedHandler(client));
+
+                // Start to accept incomming connections again 
+                GetSocket().BeginAccept(connectionCallback, null);
+            }
+            catch (Exception Exception)
+            {
+                Console.WriteLine("An error has occured while trying to accept a connecting client.\n\n{0}", Exception.Message);
+            }
+        }
+
+        /// <summary>Called when a message was recived, calls the OnMessageReceived event</summary>
+        /// <param name="Client">The client that sent the message</param>
+        /// <param name="Message">The message that the client sent</param>
+        public void ReceiveMessage(Client Client, string Message)
+        {
+            if (OnMessageReceived == null) 
+                throw new Exception("Server error: event OnMessageReceived is not bound!");
+            OnMessageReceived(this, new OnMessageReceivedHandler(Client, Message));
+        }
+
+        /// <summary>Called when a client disconnectes, calls event OnClientDisconnected</summary>
+        /// <param name="Client">The client that disconnected</param>
+        public void ClientDisconnect(Client Client)
+        {
+            // Remove the client from the connected clients list
+            _clients.Remove(Client);
+
+            // Call the OnClientDisconnected event
+            if (OnClientDisconnected == null) throw new Exception("Server error: OnClientDisconnected is not bound!");
+            OnClientDisconnected(this, new OnClientDisconnectedHandler(Client));
+        }
+
+        #endregion
+
+        #region Server Events
+
+        /// <summary>Send a message to a connected client</summary>
+        /// <param name="Client">The client to send the data to</param>
+        /// <param name="Data">The data to send the client</param>
+        public void SendMessage(Client Client, string Data)
+        {
+            // Create a websocket frame around the data to send
+            byte[] frameMessage = Helpers.GetFrameFromString(Data);
+
+            // Send the framed message to the in client
+            Client.GetSocket().Send(frameMessage);
+
+            // Call the on send message callback event 
+            if (OnSendMessage == null) throw new Exception("Server error: event OnSendMessage is not bound!");
+            OnSendMessage(this, new OnSendMessageHandler(Client, Data));
+        }
+
+        static int MaxCacheCount = 30;
+        List<byte[]> mCacheMsg = new List<byte[]>();
+        public void SendCacheMessage(Client client)
+        {
+            client.GetSocket().Send(mCacheMsg.SelectMany(i => i).ToArray());
+        }
+
+        int mLogCount = 0;
+        public void BroadcastMessage(string s, Client source = null)
+        {
+            var Data = Encoding.Convert(
+                Encoding.Default
+                , Encoding.UTF8
+                , Encoding.Default.GetBytes(string.Format("{0:0000}: {1}\r\n", ++mLogCount, s)));
+            BroadcastMessage(Data, source);
+
+            // cach last MaxCacheCount msgs
+            mCacheMsg.Add(Data);
+            if(mCacheMsg.Count > MaxCacheCount)
+            {
+                mCacheMsg.RemoveAt(0);
+            }
+        }
+        public void BroadcastMessage(byte[] Data, Client source = null)
+        {
+            foreach(var client in _clients)
+            {
+                if(client != source)
+                    client.GetSocket().Send(Data);
+            }
+        }
+
+
+        /// <summary>Called after a message was sent</summary>
+        public event EventHandler<OnSendMessageHandler> OnSendMessage;
+
+        /// <summary>Called when a client was connected to the server (after handshake)</summary>
+        public event EventHandler<OnClientConnectedHandler> OnClientConnected;
+
+        /// <summary>Called when a message was received from a connected client</summary>
+        public event EventHandler<OnMessageReceivedHandler> OnMessageReceived;
+
+        /// <summary>Called when a client disconnected</summary>
+        public event EventHandler<OnClientDisconnectedHandler> OnClientDisconnected;
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/sys/NetSys.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+using Net;
+
+/*
+    // public class UdpSocket : Socket
+    // {
+    //     public UdpSocket(SocketInformation socketInformation)
+    //     :base(socketInformation)
+    //     {}
+
+    //     public UdpSocket(SocketType socketType, ProtocolType protocolType)
+    //     :base(socketType, protocolType)
+    //     {}
+
+    //     public UdpSocket(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType)
+    //     :base(addressFamily, socketType, protocolType)
+    //     {}
+    // }
+*/
+
+public class NetSys : SingleMono<NetSys>
+{
+
+    int DebugListenPort = 7201;
+    int GameListenPort = 9988;
+    Server m_debugServer = null;
+
+
+    Server m_gameServer = null;
+
+
+    void Awake()
+    {
+        StartDebugTcpServer(DebugListenPort);
+        StartGameUdpServer(GameListenPort);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if(m_debugServer != null)
+        {
+            AppLog.OnMessageReceived += SendDebugMessage;
+        }
+
+        // var t = new System.Threading.Thread(ReceiveMessage);
+        // t.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		// perframe msg
+
+		// 1 second msg
+
+		// 5 seconds msg
+
+    }
+
+    void OnDestroy()
+    {
+        AppLog.OnMessageReceived -= SendDebugMessage;
+        if (m_debugServer != null)
+            m_debugServer.Stop();
+    }
+
+    public void StartGameUdpServer(int port)
+    {
+        var endpoint = new IPEndPoint(IPAddress.Any, port);
+        var server = new Server(endpoint, SocketType.Dgram, ProtocolType.Udp);
+        // server.GetSocket().SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+        server.GetSocket().SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 500);
+        server.OnClientConnected += (object sender, OnClientConnectedHandler e) =>
+        {
+            // server.SendCacheMessage(e.GetClient());
+            AppLog.d("Client {0} Connected", e.GetClient().GetGuid());
+        };
+
+        server.OnClientDisconnected += (object sender, OnClientDisconnectedHandler e) =>
+        {
+            AppLog.d("Client {0} Disconnected", e.GetClient().GetGuid());
+        };
+
+        server.OnMessageReceived += (object sender, OnMessageReceivedHandler e) =>
+        {
+            AppLog.d("Client {0} Message: {1}", e.GetClient().GetGuid(), e.GetMessage());
+        };
+
+        server.OnSendMessage += (object sender, OnSendMessageHandler e) =>
+        {
+            //AppLog.d("Sent message: '{0}' to client {1}", e.GetMessage(), e.GetClient().GetGuid());
+        };
+
+        m_gameServer = server;
+    }
+
+    public void SendGameMessage(byte[] data, EndPoint endPoint)
+    {
+        m_gameServer.GetSocket().SendTo(data, endPoint);
+    }
+
+    public void ReceiveDebugMessage()
+    {
+        while(true)
+        {
+            AppLog.d(" ******* ReceiveMessage ******* ");
+            if (m_debugServer != null)
+            {
+                EndPoint endpoint = new IPEndPoint(IPAddress.Any, DebugListenPort);
+                byte[] data = new byte[1024];
+                var recv = m_debugServer.GetSocket().ReceiveFrom(data, ref endpoint);
+                UnityEngine.Debug.Log("------- receive from udp: " + System.Text.Encoding.ASCII.GetString(data, 0, recv));
+            }
+            // System.Threading.Thread.Sleep(1);
+        }
+    }
+    public void SendDebugMessage(object sender, AppLog.OnMessageReceivedHandler e)
+    {
+        m_debugServer.BroadcastMessage(e.Message);
+        // var endpoint = new IPEndPoint(IPAddress.Broadcast, DebugListenPort);
+        // m_debugServer.GetSocket().SendTo(System.Text.Encoding.UTF8.GetBytes(e.Message), endpoint);
+    }
+    public void StartDebugTcpServer(int port)
+    {
+        var endpoint = new IPEndPoint(IPAddress.Any, port);
+        // var server = new TcpServer.Server(endpoint, SocketType.Dgram, ProtocolType.Udp);
+        var server = new Net.Server(endpoint);
+        // server.GetSocket().SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+        // server.GetSocket().SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 5000);
+        server.OnClientConnected += (object sender, OnClientConnectedHandler e) =>
+		{
+            server.SendCacheMessage(e.GetClient());
+        };
+
+        server.OnClientDisconnected += (object sender, OnClientDisconnectedHandler e) =>
+        {
+            AppLog.d("Client {0} Disconnected", e.GetClient().GetGuid());
+        };
+
+        server.OnMessageReceived += (object sender, OnMessageReceivedHandler e) =>
+        {
+            // server.GetSocket().SendTo(System.Text.Encoding.UTF8.GetBytes(e.GetMessage()), endpoint);
+			server.BroadcastMessage(System.Text.Encoding.UTF8.GetBytes(string.Format("{0}:\n\t{1}", e.GetClient().GetGuid(), e.GetMessage())), e.GetClient());
+        };
+
+        server.OnSendMessage += (object sender, OnSendMessageHandler e) =>
+        {
+			//AppLog.d("Sent message: '{0}' to client {1}", e.GetMessage(), e.GetClient().GetGuid());
+		};
+
+        m_debugServer = server;
+        
+    }
+}
