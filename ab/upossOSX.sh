1734b2d6de0530523e3a21074b640e9332642326
cn <cool_navy@qq.com>
2021-02-24 18:49:59 +0800

    up uposs

--- ab/upossOSX.sh
+++ ab/upossOSX.sh
@@ -1,1 +1,2 @@
+cd `dirname $0`
 ossutil cp -ru /Volumes/Data/test/ab/OSX oss://a3-dev-patch-oss/test/a3mkgp/OSX/1.0.0 --exclude *.manifest --exclude *user.*
--- ab/upossWin.sh
+++ ab/upossWin.sh
@@ -1,1 +1,3 @@
+
+cd `dirname $0`
 ossutil cp -ru /Volumes/Data/test/ab/Windows oss://a3-dev-patch-oss/test/a3mkgp/Windows/1.0.0 --exclude *.manifest --exclude *user.*
